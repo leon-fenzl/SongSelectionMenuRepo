32eb2b3ec13187e37dd29a1a94f1d3518a22a203
León Fenzl <32179683+leon-fenzl@users.noreply.github.com>
2022-10-19 11:19:34 -0300

    FinalUpdate

--- TestUI/Scripts/ButtonsBehaviours/SongButtonBehaviour.gd
+++ TestUI/Scripts/ButtonsBehaviours/SongButtonBehaviour.gd
@@ -20,7 +20,7 @@ func SetValues():
 			currentStream = load(testSongsPath+songArray[buttonIndex])
 			aSPlayer.stream = currentStream
 func _on_custom_base_song_button_mouse_entered():
-	custom_minimum_size = Vector2(custom_minimum_size.x,120)
+	custom_minimum_size = Vector2(custom_minimum_size.x,120.0)
 func _on_custom_base_song_button_mouse_exited():
 	custom_minimum_size = Vector2(custom_minimum_size.x,100.0)
 func _on_line_edit_2_text_submitted(new_text):
--- TestUI/Scripts/GManager.gd
+++ TestUI/Scripts/GManager.gd
@@ -4,16 +4,15 @@ extends Control
 @onready var buttonObject ="res://Buttons/SongButton.tscn"
 @onready var buttonsArray = []
 @onready var searchMatches = []
-@onready var vbcBttHolder = get_node("Menu/HBC_Divider/ScrollContainer/VBC_Buttons")
+@onready var vbcBttHolder =$CanvasLayer/HBoxContainer/ScrollContainer/VBC_Buttons
 func _ready():
 	CreateSongsArray(songsFolderPath)
 	SpawnButtons()
 	FeedEachButton()
-	
+
 func _input(_event):
 	if Input.is_action_just_pressed("ui_cancel"):
 		get_tree().quit()
-
 func CreateSongsArray(path):
 	var dir = DirAccess.open(path)
 	dir.list_dir_begin()
@@ -25,15 +24,14 @@ func CreateSongsArray(path):
 			songSArray.append(songFile)
 	dir.list_dir_end()
 #	return songSArray
-
 func SpawnButtons():
 	var object = load(buttonObject)
 	for i in songSArray:
 		var bObj = object.instantiate()
 		buttonsArray.append(bObj)
 		vbcBttHolder.add_child(bObj)
-#	return buttonsArray
-
+		if i not in songSArray:
+			return
 func FeedEachButton():
 	for i in buttonsArray.size():
 		buttonsArray[i].buttonIndex = i
@@ -44,7 +42,7 @@ func FeedEachButton():
 		upperCase = upperCase.left(1).to_upper()
 		lowerCase = lowerCase.right(-1).replace(".ogg","")
 		buttonsArray[i].text = upperCase+lowerCase
-		
+
 func _on_search_bar_text_changed(new_text):
 	new_text = new_text.to_lower()
 	if new_text == "":
@@ -52,9 +50,9 @@ func _on_search_bar_text_changed(new_text):
 			i.show()
 		return
 	for i in buttonsArray:
-		if(new_text in i.text.to_lower()
-		|| new_text in i.album.text.to_lower()
-		|| new_text in i.artist.text.to_lower()
+		if(new_text in i.text.to_lower() 
+		|| new_text in i.album.text.to_lower() 
+		|| new_text in i.artist.text.to_lower() 
 		|| new_text in i.group.text.to_lower()):
 			searchMatches.append(i)
 		if i in searchMatches:i.show()
